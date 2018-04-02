a496ef39ee6236ef8e603ba9671c9c9d7d03783d
Tomokatsu Iguchi <tomo14916253@gmail.com>
2018-04-02 23:18:10 +0900

    done

--- Assets/BasketController.cs
+++ Assets/BasketController.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class BasketController : MonoBehaviour {
 
+	// アウトレット接続
+	public AudioClip appleSE;
+	public AudioClip bombSE;
+	AudioSource aud;
+	GameObject director;
+
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Apple") {
 			// りんご
+			this.aud.PlayOneShot(this.appleSE);
+			this.director.GetComponent<GameDirector>().GetApple();
 		} else {
 			// 爆弾
+			this.aud.PlayOneShot(this.bombSE);
+			this.director.GetComponent<GameDirector>().GetBomb();
 		}
 		Destroy (other.gameObject); // otherはぶつかってきたItemのコライダー(other.gameObjectでGameObjectの参照ができる)
 	}
 
 	// Use this for initialization
 	void Start () {
-		
+		this.aud = GetComponent<AudioSource> (); // GUI上で音楽を直接指定するのではなく、スクリプトで。複数の音源が使えるように。
+		this.director = GameObject.Find("GameDirector");
 	}
 	
 	// Update is called once per frame
